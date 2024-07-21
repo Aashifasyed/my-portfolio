5bb96c3f7f3ea1c4a58fe61142b39c8ff375b0c7
Aashifasyed <147689681+Aashifasyed@users.noreply.github.com>
2024-07-21 15:59:10 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// scripts.js
+
+// Function to display an alert
+document.getElementById('alertButton').addEventListener('click', function() {
+    alert('Welcome to the Hobbies Website!');
+});
+
+// Function to change the background color of the page
+document.getElementById('colorButton').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'lightblue';
+});
